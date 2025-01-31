8ecda2b99efec072be142d60ab9c722bdb9dd4c1
BR0F3SS0R <aphisit.pra@ku.th>
2025-02-01 02:44:05 +0700

    Edit Read Me

--- src/app/hello/page.tsx
+++ src/app/hello/page.tsx
@@ -1,3 +1,4 @@
 export default function HelloPage() {
-    return <div>Hello, World!</div>;
+    return <div className="text-3xl font-bold underline p-4 border-2 justify-center flex"
+    >Hello, World!</div>;
   }
